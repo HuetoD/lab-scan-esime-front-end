bc593b8227a8488c916dee06e6fb0044cfd30b18
Mictlan <kevinw130501@gmail.com>
2023-11-27 01:01:32 -0600

    [added]: material ui styles

--- lab-scan/src/app/app.module.ts
+++ lab-scan/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'
 //import { FeatureModule } from './features/feature.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InicioSesionComponent } from'./pages/auth/login/inicio-sesion/inicio-sesion.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -20,6 +22,8 @@ import { InicioSesionComponent } from'./pages/auth/login/inicio-sesion/inicio-se
     AppRoutingModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
     //FeatureModule
   ],
   providers: [],
