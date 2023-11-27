8e671d882e341ec24f601146c3e74b743b545ade
Alan CR <alan@ipn.com>
2023-11-27 00:45:13 -0600

    Update: Components login

--- lab-scan/src/app/app.module.ts
+++ lab-scan/src/app/app.module.ts
@@ -6,13 +6,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InicioSesionComponent } from'./pages/auth/login/inicio-sesion/inicio-sesion.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioSesionComponent
+    LoginComponent
     
   ],
   imports: [
