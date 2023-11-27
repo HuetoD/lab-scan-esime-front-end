469d2ea5e866b8aa150a8f10e392f8a8ecd2d923
Alan CR <alan@ipn.com>
2023-11-27 00:15:09 -0600

    Added: New components login

--- lab-scan/src/app/app.module.ts
+++ lab-scan/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+//import { FeatureModule } from './features/feature.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FeatureModule } from './features/feature.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioSesionComponent } from'./pages/auth/login/inicio-sesion/inicio-sesion.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioSesionComponent
+    
   ],
   imports: [
-    FeatureModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    //FeatureModule
   ],
   providers: [],
   bootstrap: [AppComponent]
