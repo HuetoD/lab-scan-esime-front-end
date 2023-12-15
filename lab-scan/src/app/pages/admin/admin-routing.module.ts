4e3215538f96bf1365a1e0d19942e1d08b4c5527
Alan CR <alan@ipn.com>
2023-12-14 19:12:17 -0600

    Added adminis components and delete note module xd

--- lab-scan/src/app/pages/admin/admin-routing.module.ts
+++ lab-scan/src/app/pages/admin/admin-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AdminComponent } from "./admin/admin.component";
+import { AdminisComponent } from './adminis/adminis.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AdminComponent
+  },
+  {
+    path: 'adminis',
+    component: AdminisComponent
   }
 ];
 
