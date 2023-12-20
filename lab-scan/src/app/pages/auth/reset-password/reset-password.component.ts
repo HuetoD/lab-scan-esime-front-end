728f7df252c4d832b7d95139d7a6e22e2965d6be
Daniel Perez <60562714+iCyclo@users.noreply.github.com>
2023-12-20 17:07:41 -0600

    fixed: merge conflicts

--- lab-scan/src/app/pages/auth/reset-password/reset-password.component.ts
+++ lab-scan/src/app/pages/auth/reset-password/reset-password.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from "@angular/core"
+import { FormControl, Validators } from "@angular/forms"
+import { AuthService } from "src/app/services/auth.service"
+
 
 @Component({
     selector: 'app-reset-password',
